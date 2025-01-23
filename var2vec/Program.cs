b0a80af842f9a778df6229a4afbccf0c8d7118f8
valpen094 <welciei3524@gmail.com>
2025-01-23 22:28:24 +0900

    add var2vec

---
+++ var2vec/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OllamaSharp;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Please specify the target directory.");
+    Console.ReadLine();
+}
+else
+{
+    string dir = args[0];
+    if (!Directory.Exists(dir))
+    {
+        Console.WriteLine("The specified directory does not exist.");
+        Console.ReadLine();
+        return;
+    }
+
+    var uri = new Uri("http://localhost:11434");
+    var client = new OllamaApiClient(uri)
+    {
+        SelectedModel = "phi3"
+    };
+
+    var models = await client.ListLocalModelsAsync();
+    Console.WriteLine($"Connecting to {uri} ...");
+
+    await foreach (var status in client.PullModelAsync("phi3"))
+    {
+        Console.WriteLine($"{status.Percent}% {status.Status}");
+    }
+
+    Console.WriteLine();
+
+    string[] csvFiles = Directory.GetFiles(dir, "*.csv", SearchOption.AllDirectories);
+    string converted = string.Empty;
+    foreach (var csvFile in csvFiles)
+    {
+        var result = await client.EmbedAsync(csvFile);
+        List<float[]> vectors = result.Embeddings;
+
+        foreach (var embedding in result.Embeddings)
+        {
+            foreach (var value in embedding)
+            {
+                Console.Write($"{value:F6} ");
+                converted += $"{value:F6} ";
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    File.WriteAllText($"{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.txt", converted);
+    Console.WriteLine("\r\nFinished.");
+}
